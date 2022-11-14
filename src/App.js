3ef63d5f1b2382df1c0e8013295cf6bd358e2379
shrestha-ramesh <shresthashare@gmail.com>
2022-11-13 18:13:37 -0600

    Added pipeline for PR and main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Top">
+        <div className="One">
+          <div className="Circle1">1</div>
+          <div className="Circle2">2</div>
+          <div className="Circle3">3</div>
+        </div>
+        <div className="Four">
+          <div className="Two">tap</div>
+          <div className="Three"> dropdown</div>
+        </div>
+      </div>
+      <div className="Search">jhkjhjhl</div>
+      <div className="Main"></div>
     </div>
   );
 }
